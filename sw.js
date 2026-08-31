129c5eda1a8b04d56ac59d5b6d309a0022c424a2
jp024160 <48156667+jp024160@users.noreply.github.com>
2026-08-31 23:41:08 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dango-pos-v5-1-20260831';
+const CACHE='dango-pos-v5-2-20260831';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
