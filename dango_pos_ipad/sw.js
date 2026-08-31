2186e37db0a986d67c1bd2c40513ae40f3f9783b
jp024160 <48156667+jp024160@users.noreply.github.com>
2026-08-31 23:15:30 +0900

    Add files via upload

--- dango_pos_ipad/sw.js
+++ dango_pos_ipad/sw.js
@@ -1,4 +1,4 @@
-const CACHE='dango-pos-v4-1-20260831';
+const CACHE='dango-pos-v5-20260831';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
